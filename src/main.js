3d25286e161630f5760d30f0f53c5979311adc7e
Administrator <460028466@qq.com>
2022-10-21 22:18:55 +0800

    commit first

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  // render(createElement){
+//    return createElement("h1", "你好啊")
+  // }
+  // template:<h1>你好啊</h1>
+  // components:{App}
+}).$mount('#app')
