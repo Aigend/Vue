b8e124ba5506e836e6f77306aca0e7ae985e68d0
wenlong <460028466@qq.com>
2022-12-24 21:27:07 +0800

    commit code

--- leetcode/JZ10 斐波那契数列.py
+++ leetcode/JZ10 斐波那契数列.py
@@ -20,5 +20,18 @@ class Solution:
         return self.Fibonacci(n-1) + self.Fibonacci(n-2)
 
 
+class Solution2:
+    def Fibonacci(self , n: int) -> int:
+        # write code here
+        dp = [0] * n
+        if n < 3:
+            return 1
+        dp[0] = 1
+        dp[1] = 1 
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n-1]
+
+
 if __name__ == '__main__':
     print(Solution().Fibonacci(36))
